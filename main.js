095687c09aca4722181a0ee159c91aebdb140a97
siva <sivasebi4545@gmail.com>
2023-07-19 16:43:38 +0530

    twodigitenumberarray

---
+++ main.js
@@ -0,0 +1,12 @@
+var arr=[2,33,45,7,34,7,35,8];
+
+document.write("Giver the array Element="+arr+"<br>");
+document.write("Two digite number is"+"<br>");
+for(let i=0;i<arr.length;i++){
+	if((arr[i]>9)&&(arr[i]<100))
+	{
+		
+ document.write( arr[i]+"<br>");
+ 
+}
+}
